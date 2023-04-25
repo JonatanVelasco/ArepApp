43202a2fa16754155d64e28ab517a0c11f673818
Jonatan Velasco <106565107+JonatanVelasco@users.noreply.github.com>
2023-04-24 20:23:43 -0500

    Add files via upload
    First commit for create repository

---
+++ src/arepappclase/ArepAppClase.java
@@ -0,0 +1,16 @@
+package arepappclase;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class ArepAppClase {
+
+    public static void main(String[] args) {
+       
+    }
+}
